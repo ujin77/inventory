bb886d6cd0959e92c993c46377edc94835dda8b4
Eugene M <ujin@i.ua>
2018-07-23 11:43:22 +0300

    Add order and vendor filter

--- invent_app/views/routes.py
+++ invent_app/views/routes.py
@@ -3,28 +3,52 @@
 #
 from invent_app import app, db, excel
 from invent_app.models import Host, Soft
-from flask import render_template
-from sqlalchemy import func
+from flask import render_template, request
+from sqlalchemy import func, desc
 from sqlalchemy.sql import label
-import urllib2
+# import urllib2
+
+
+def get_request_order(default_order=''):
+    order_by = request.args.get('order_by')
+    order = request.args.get('order')
+    if not order_by:
+        order_by = default_order
+    if order:
+        order_by = desc(order_by)
+        order = None
+    else:
+        order = 'desc'
+
+    return order_by, order
 
 
 @app.route('/')
 @app.route('/index')
 @app.route('/hosts')
 def hosts():
-    data = Host.query.order_by(Host.Hostname).all()
-    return render_template('index.html', title='Hosts', data=data)
+    (order_by, order) = get_request_order('Hostname')
+    data = Host.query.order_by(order_by).all()
+    return render_template('index.html', title='Hosts', data=data, order=order)
 
 
 @app.route('/all_software')
 def all_software():
-    data = db.session.query(Soft.Name,
-                            Soft.Version,
-                            Soft.Vendor,
-                            label('Count', func.count(Soft.Name)),
-                            Soft.IsUpdate).group_by(Soft.Name).all()
-    return render_template('all_software.html', title='Software', data=data)
+    (order_by, order) = get_request_order('Name')
+    vendor = request.args.get('vendor')
+    if vendor:
+        data = db.session.query(Soft.Name,
+                                Soft.Version,
+                                Soft.Vendor,
+                                label('Count', func.count(Soft.Name)),
+                                Soft.IsUpdate).group_by(Soft.Name).filter_by(Vendor=vendor).order_by(order_by)
+    else:
+        data = db.session.query(Soft.Name,
+                                Soft.Version,
+                                Soft.Vendor,
+                                label('Count', func.count(Soft.Name)),
+                                Soft.IsUpdate).group_by(Soft.Name).order_by(order_by).all()
+    return render_template('all_software.html', title='Software', data=data, order=order, vendor=vendor)
 
 
 @app.route('/software/<name>')
@@ -35,8 +59,13 @@ def software(name):
 
 @app.route('/host/<h_name>')
 def host(h_name):
-    data = Soft.query.filter_by(Hostname=h_name).order_by(Soft.Name)
-    return render_template('host.html', title=h_name, data=data)
+    (order_by, order) = get_request_order('Name')
+    vendor = request.args.get('vendor')
+    if vendor:
+        data = Soft.query.filter_by(Hostname=h_name, Vendor=vendor).order_by(order_by)
+    else:
+        data = Soft.query.filter_by(Hostname=h_name).order_by(order_by)
+    return render_template('host.html', title=h_name, data=data, order=order, vendor=vendor)
 
 
 @app.route('/inventory_software.csv', methods=['GET'])
